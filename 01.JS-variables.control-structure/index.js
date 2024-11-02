1f83f6f7e3615f256541329b13fd30235e4e7536
VusalaAli <vusaleelizade2004@gmail.com>
2024-11-02 11:52:41 +0400

    added

---
+++ 01.JS-variables.control-structure/index.js
@@ -0,0 +1,116 @@
+// let x =prompt("ededi daxil edin");
+
+// for (let i =0; i <=x; i++) {
+//     if(x % i ===0){
+//         console.log(i);
+        
+//     }
+    
+    
+// }
+
+
+
+
+
+
+
+
+// let x =prompt("ededi daxil edin");
+// let count =0;
+// for (let i =0; i <=x; i++) {
+//     if(x % i ===0){
+//         count++
+//     }
+    
+    
+// }console.log(count);
+
+
+
+
+
+
+
+
+// let i = 0 ;
+// for (let i =0; i <=10; i++) {
+//   let i2 = i**2; 
+//   let i3 = i**3; 
+//   console.log(i, i2, i3);
+// }
+
+
+
+
+
+
+
+
+
+
+// for (let num = 2; num <= 100; num++) {
+//     let prime = true;
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) {
+//             prime = false;   
+//         }
+//     }
+
+//     if (primre) {
+//         console.log(num);
+//     }
+// }
+// let isPrime = true;
+// for (let i = 2; i < Math.sqrt(n); i++) {
+//   if (n % i === 0) {
+//     isPrime = false;
+//   }
+// }
+
+
+
+
+
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i * i === i**2);
+// }
+
+
+
+
+
+
+// let array =[1,2,3,4,5,6,7,8,9]
+// let x =prompt("ededi daxil edin");
+
+// for (let i = 0; i < 9; i++) {
+//    if( x === array){
+//     console.log("eded array' de var");
+    
+//    }else if(x != array){
+//     console.log("eded array' de yoxdur");
+    
+//    }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
